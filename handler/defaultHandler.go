b2d70ab8726bf88909a8c9033b13467de414ca76
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-03-05 13:47:11 +0100

    Update root html site to include information about new endpoints

--- handler/defaultHandler.go
+++ handler/defaultHandler.go
@@ -22,8 +22,10 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 	// Information to display to user on root path
 	outout := "This website offers three services.<br><a href=\"" +
 		DIAG_PATH + "\">" + DIAG_PATH + "</a> - Diagnostic interface<br> <a href=\"" + UNIINFI_PATH +
-		"\">" + UNIINFI_PATH + "</a> - University information<br> <a href=\"" + NEIGHBOURUNIS_PATH +
-		"\">" + NEIGHBOURUNIS_PATH + "</a> - Neighbour universities"
+		"\">" + UNIINFI_PATH + "</a> - University information by name<br> <a href=\"" + NEIGHBOURUNIS_PATH +
+		"\">" + NEIGHBOURUNIS_PATH + "</a> - University information by neighbour country and name<br> <a href=\"" + COUNTRYUNIS_PATH +
+		"\">" + COUNTRYUNIS_PATH + "</a> - University information by country and name<br> <a href=\"" + COUNTRYALLUNIS_PATH +
+		"\">" + COUNTRYALLUNIS_PATH + "</a> - University information by country"
 
 	// Write information to client
 	_, err := fmt.Fprintf(w, "%v", outout)
